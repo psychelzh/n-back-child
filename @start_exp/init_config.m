3eaf9a12863a0446913ab0d5a513a178b37f9350
Liang Zhang <psychelzh@outlook.com>
2018-12-16 15:12:25 +0800

    Complete sequence configuration

--- @start_exp/init_config.m
+++ @start_exp/init_config.m
@@ -1,113 +1,110 @@
-function init_config(app)
+function config = init_config(app, part)
 %GENSEQ generates sequence for current study and stores in a `.csv` file
+%
+%   The basic thing to do here is to generate sequence for our experiment
+%   test, which contains two parts: practice (part: 'prac') and testing
+%   (part: 'test'). Generally, a **fixed** pseudorandom sequence is
+%   required for testing part but not for practice part. Thus, the random
+%   number generation of testing part seed is set as 0 for each user,
+%   whereas that of practice part is set as 'Shuffle'.
 
-% set random seed as the same to ensure the same sequence is generated
-rng(0)
-% initialize configurtion
-config = struct;
-config.experiment_name = app.ExperimentName;
-parts = ["prac", "test"];
-for part = parts
-    % configure block sequence
-    task_names = ["back0", "back1", "back2"]';
-    switch part
-        case 'prac'
-            block_names = task_names;
-        case 'test'
-            block_names = perms(task_names);
-            block_names = block_names(:);
-    end
-    % generate sequence for each block
+% set all the task names
+task_names = ["zero-back", "one-back", "two-back"];
+disp_names = ["0¼¶", "1¼¶", "2¼¶"];
+map_names = containers.Map(task_names, disp_names);
+% configure 
+switch part
+    case 'prac'
+        % set different random seed for each user
+        rng('Shuffle')
+        run_blocks = {task_names};
+    case 'test'
+        % fix random seed to ensure sequences for all users are the same
+        rng(0)
+        % testing part has two runs, and each run has three blocks
+        block_names_regular = perms(task_names)';
+        run_blocks = cellfun(@(x) x(:), ...
+            {block_names_regular(:, 1:3), block_names_regular(:, 4:6)}, ...
+            'UniformOutput', false);
+end
+% generate sequence for each run and each block
+num_runs = length(run_blocks);
+config.runs = repelem(struct, num_runs);
+for i_run = 1:num_runs
+    block_names = run_blocks{i_run};
     num_blocks = length(block_names);
-    num_trial_each_block = 10;
-    seq_part = repelem(struct, num_blocks);
+    config.runs(i_run).blocks = repelem(struct, num_blocks);
     for i_block = 1:num_blocks
         block_name = block_names(i_block);
-        [seq_stim, seq_type] = ...
-            genblockseq(num_trial_each_block, block_name);
-        seq_part(i_block).block_id = i_block;
-        seq_part(i_block).block_name = block_name;
-        seq_part(i_block).seq_trial.stim = seq_stim;
-        seq_part(i_block).seq_trial.type = seq_type;
+        config.runs(i_run).blocks(i_block).id = i_block;
+        config.runs(i_run).blocks(i_block).name = block_name;
+        config.runs(i_run).blocks(i_block).disp_name = map_names(block_name);
+        config.runs(i_run).blocks(i_block).trials = ...
+            gen_block_seq(app.StimuliSet, app.NumberTrialsPerBlock, block_name);
     end
-    config.sequence.(part) = seq_part;
 end
-% output as json config file
-config_file = fopen(app.ConfigFileName, 'w');
-fprintf(config_file, '%s', jsonencode(config));
-fclose(config_file);
-% restore seed for random number generation to default
 rng('default')
 end
 
-function [seq_stim, seq_type] = genblockseq(num_trials, task_name)
+function seq = gen_block_seq(stim_set, num_trials, task_name)
 % GENBLOCKSEQ generates sequence for each block
 
-% stimuli set
-stim_set = 0:9;
+% configurations
+trial_types = ["filler", "target", "distractor"];
 % generate trial type sequence
-trial_types = ["target", "distractor"];
-seq_type = sample(repelem(trial_types, num_trials / length(trial_types)));
+type_population = repelem(trial_types(trial_types ~= "filler"), ...
+    num_trials / sum(trial_types ~= "filler"));
+type = randsample(type_population, length(type_population));
+% preallocate
+stim = nan(1, length(type));
 % generate sequence
 switch task_name
-    case "back0"
+    case "zero-back"
         % "target" is numbers smaller than 5, and "distractor" otherwise
-        seq_stim(seq_type == "target") = sample(stim_set(stim_set < 5));
-        seq_stim(seq_type == "distractor") = sample(stim_set(stim_set >= 5));
-    case "back1"
+        stim(type == "target") = randsample(stim_set(stim_set < 5), sum(stim_set < 5));
+        stim(type == "distractor") = randsample(stim_set(stim_set >= 5), sum(stim_set < 5));
+    case "one-back"
         % the first should be "filler"
-        seq_type(1) = "filler";
-        seq_stim = nan(1, num_trials);
+        type(1) = "filler";
         for itrial = 1:num_trials
-            if seq_type(itrial) == "filler"
-                seq_stim(itrial) = sample(stim_set, 1);
+            if type(itrial) == "filler"
+                stim(itrial) = randsample(stim_set, 1);
             end
-            if seq_type(itrial) == "target"
-                seq_stim(itrial) = seq_stim(itrial - 1);
+            if type(itrial) == "target"
+                stim(itrial) = stim(itrial - 1);
             end
-            if seq_type(itrial) == "distractor"
-                seq_stim(itrial) = sample(stim_set, 1, seq_stim(itrial - 1));
+            if type(itrial) == "distractor"
+                stim(itrial) = randsample(stim_set, 1, true, ~ismember(stim_set, stim(itrial - 1)));
             end
         end
-    case "back2"
+    case "two-back"
         % the first two trials should be "filler"
-        seq_type(1:2) = "filler";
-        seq_stim = nan(1, num_trials);
+        type(1:2) = "filler";
         for itrial = 1:num_trials
-            if seq_type(itrial) == "filler"
+            if type(itrial) == "filler"
                 if itrial == 1
                     exclude = [];
                 else
-                    exclude = seq_stim(itrial - 1);
+                    exclude = stim(itrial - 1);
                 end
-                seq_stim(itrial) = sample(stim_set, 1, exclude);
+                stim(itrial) = randsample(stim_set, 1, true, ~ismember(stim_set, exclude));
             end
-            if seq_type(itrial) == "target"
-                seq_stim(itrial) = seq_stim(itrial - 2);
+            if type(itrial) == "target"
+                stim(itrial) = stim(itrial - 2);
             end
-            if seq_type(itrial) == "distractor"
-                seq_stim(itrial) = sample(stim_set, 1, seq_stim(itrial - 2:itrial - 1));
+            if type(itrial) == "distractor"
+                stim(itrial) = randsample(stim_set, 1, true, ~ismember(stim_set, stim(itrial - 2:itrial - 1)));
             end
         end
     otherwise
         error('NBACKCHILD:init_cofig:genblockseq:invalidTaskName', ...
             'Task name ''%s'' is not supported now.', task_name);
 end
-end
-
-function [spl, idx] = sample(set, k, exclude)
-% SAMPLE randomly selects from SET without replacement
-
-if nargin < 3
-    exclude = [];
-end
-if ~isempty(exclude)
-    set = setdiff(set, exclude);
-end
-length_set = length(set);
-if nargin < 2
-    k = length(set);
-end
-idx = randperm(length_set, k);
-spl = set(idx);
+% set the correct response sequence
+cresp = strings(1, length(type));
+cresp(type == "target") = "Left";
+cresp(type == "distractor") = "Right";
+% compose output structure
+seq = struct('id', num2cell(1:num_trials), 'type', num2cell(type), ...
+    'cresp', num2cell(cresp), 'stim', num2cell(stim));
 end
